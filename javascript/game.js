ea5cdf36bdf2af2a78ca0390dc01320802454546
Bridget Bourne <bbourne8891@hotmail.com>
2018-02-08 22:35:29 -0500

    Struggling to prevent player reselection. I need something to make sure the playerSelect function can never be repeated.

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	
 	$("img").click(function playerSelect(){
 		var nPC = $("img").css("border", "none");
 		var charSelect = $(this).css("border", "5px solid orange");
@@ -11,21 +12,36 @@ $(document).ready(function(){
 		console.log("The player has now selected his/her fighter.");
 		$("p").remove();
 		$("h3").remove();
+		var player = $(this).addClass("player");
 
-		// charSelect.css("float", "left top");
-		// nPC.css("float", "right");
-		// $("#game-message").append("Now you must choose an opponent to fight, and continue to fight until your candidate either dies or becomes the last man standing.");
-		// $("#game-message").css("width", 1);
+		//Now, make sure that the player character can't be reselected.
+
+		var classy = $("img").attr("class");
+		if (classy !== "player"){
+			$("img").addClass("opponent");
+		};//End if(classy) conditional
+
 	});//End player select feature
 	// $("img").click(function);
-	$("img").click(function opponentSelect(){
-		charSelect = $(this).css("border", "5px solid green");
-		console.log("A green frame delicately embraces the illustration.")	
-		var areYouSure = confirm(confirm("Will this be next opponent? You may halt a battle and retreat, but neither you nor any of your opponents may recover health at any point in the game."));
-		if(areYourSure == false){
-			$("img").css("border", "none");
-		};
-		alert("Okay!" + /*Opponent name*/ + "is now your current opponent.");
-		console.log("The player has now selected his/her opponent.");
-	});
-});
+	if(playerSelect = true){
+		$(".opponent").click(function opponentSelect(){
+			if($("img").attr("class") === "opponent"){
+				var charSelect = $(this).css("border", "5px solid green");
+				console.log("A green frame delicately embraces the illustration.")	
+				var areYouSure = confirm(confirm("Will this be next opponent? You may halt a battle and retreat, but neither you nor any of your opponents may recover health at any point in the game."));
+				if(areYourSure == false){
+					$("img").css("border", "none");
+				};
+				alert("Okay!" + /*Opponent name*/ + "is now your current opponent.");
+				console.log("The player has now selected his/her opponent.");
+			};//End if($("img")) statement	
+		});	//End opponentSelect()	
+	};//End if($(".player")) conditional
+
+	/*Redeclaring jQuery events does not create 2 new function, it just redefines the same event. That's why the code thought the selected player was also the selected opponent. Place event in separate functions, not vice versa.*/
+
+
+
+
+
+});//End document.ready() jQuery thingy.
